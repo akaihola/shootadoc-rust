38e56b3028d72a07b8a20ea6b969016211810361
Filipp Samoilov <filipp.samoilov@wartsila.com>
2020-11-14 01:47:25 +0200

    Experimenting with imageproc

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,7 @@
-use image::{open, GenericImageView, GrayImage};
-
-fn brightest(img1: &GrayImage, img2: &GrayImage) -> GrayImage {
-    GrayImage::from_fn(img1.width(), img1.height(), |x, y| {
-        let p1 = img1.get_pixel(x, y);
-        let p2 = img2.get_pixel(x, y);
-        if p1.0[0] < p2.0[0] {
-            *p2
-        } else {
-            *p1
-        }
-    })
-}
+use image::open;
 
 fn main() {
-    let mut img = open("/tmp/paper.jpg").unwrap().grayscale().to_luma();
-    for round in 0..5 {
-        let offset = 2 ^ round;
-        let win_width = img.width() - offset;
-        let win_height = img.height() - offset;
-        let orig = img.view(0, 0, win_width, win_height);
-        let left = img.view(offset, 0, win_width, win_height);
-        let up = img.view(0, offset, win_width, win_height);
-        img = brightest(
-            &brightest(&orig.to_image(), &left.to_image()),
-            &brightest(&orig.to_image(), &up.to_image()),
-        );
-    }
-    img.save("/tmp/paper.png").unwrap();
+    let img = open("journal.jpg").unwrap().grayscale().to_luma();
+    let result = imageproc::contrast::adaptive_threshold(&img, 150);
+    result.save("out.jpg").unwrap();
 }
