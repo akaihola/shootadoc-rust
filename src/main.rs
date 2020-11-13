ea6a55acf0cc96cb6be2a8806f3e51a4eb31ad54
Antti Kaihola <antti.kaihola@eniram.fi>
2020-11-13 20:22:44 +0200

    Detect rounds from image size. Fix exponentiation.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
 use image::{open, GenericImageView, GrayImage};
+use std::cmp::min;
+
+const fn num_bits<T>() -> usize {
+    std::mem::size_of::<T>() * 8
+}
+
+fn log_2(x: u32) -> u32 {
+    assert!(x > 0);
+    num_bits::<u32>() as u32 - x.leading_zeros() - 1
+}
 
 fn brightest(img1: &GrayImage, img2: &GrayImage) -> GrayImage {
     GrayImage::from_fn(img1.width(), img1.height(), |x, y| {
@@ -14,17 +24,16 @@ fn brightest(img1: &GrayImage, img2: &GrayImage) -> GrayImage {
 
 fn main() {
     let mut img = open("/tmp/paper.jpg").unwrap().grayscale().to_luma();
-    for round in 0..5 {
-        let offset = 2 ^ round;
+    let smaller_extent = min(img.width(), img.height());
+    let rounds = log_2(smaller_extent) - 1;
+    for round in 0..rounds {
+        let offset = 2u32.pow(round);
         let win_width = img.width() - offset;
         let win_height = img.height() - offset;
-        let orig = img.view(0, 0, win_width, win_height);
-        let left = img.view(offset, 0, win_width, win_height);
-        let up = img.view(0, offset, win_width, win_height);
-        img = brightest(
-            &brightest(&orig.to_image(), &left.to_image()),
-            &brightest(&orig.to_image(), &up.to_image()),
-        );
+        let orig = img.view(0, 0, win_width, win_height).to_image();
+        let left = img.view(offset, 0, win_width, win_height).to_image();
+        let up = img.view(0, offset, win_width, win_height).to_image();
+        img = brightest(&brightest(&orig, &left), &brightest(&orig, &up));
     }
     img.save("/tmp/paper.png").unwrap();
 }
