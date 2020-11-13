26e7bc418aa872f32b5999beb4ac1c9bccca0698
Antti Kaihola <antti.kaihola@eniram.fi>
2020-11-13 20:06:03 +0200

    Fix vertical offset. Do 5 rounds.

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,25 @@ fn brightest(img1: &GrayImage, img2: &GrayImage) -> GrayImage {
         let p2 = img2.get_pixel(x, y);
         if p1.0[0] < p2.0[0] {
             *p2
-        } else { *p1 }
-    } )
-
+        } else {
+            *p1
+        }
+    })
 }
 
 fn main() {
-    let img = open("/tmp/paper.jpg").unwrap().grayscale().to_luma();
-    let offset = 50;
-    let win_width = img.width() - offset;
-    let win_height = img.height() - offset;
-    let orig = img.view(0, 0, win_width, win_height);
-    let left = img.view(offset, 0, win_width, win_height);
-    let up = img.view(offset, 0, win_width, win_height);
-    let b = brightest(
-        &brightest(&orig.to_image(), &left.to_image()),
-        &brightest(&orig.to_image(), &up.to_image()),
-    );
-    b.save("/tmp/paper.png").unwrap();
+    let mut img = open("/tmp/paper.jpg").unwrap().grayscale().to_luma();
+    for round in 0..5 {
+        let offset = 2 ^ round;
+        let win_width = img.width() - offset;
+        let win_height = img.height() - offset;
+        let orig = img.view(0, 0, win_width, win_height);
+        let left = img.view(offset, 0, win_width, win_height);
+        let up = img.view(0, offset, win_width, win_height);
+        img = brightest(
+            &brightest(&orig.to_image(), &left.to_image()),
+            &brightest(&orig.to_image(), &up.to_image()),
+        );
+    }
+    img.save("/tmp/paper.png").unwrap();
 }
