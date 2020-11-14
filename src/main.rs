5e857f54f2fa87f93fac9532b8b79ae37c7fe0b3
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2020-11-14 19:40:27 +0200

    WIP: trying to use views to prevent image copies

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use image::{open, GenericImageView, GrayImage};
+use image::{open, GenericImage, GenericImageView, GrayImage, Luma, Pixel};
 use std::cmp::min;
 
 mod cli;
@@ -12,14 +12,15 @@ fn log_2(x: u32) -> u32 {
     num_bits::<u32>() as u32 - x.leading_zeros() - 1
 }
 
-fn brightest(img1: &GrayImage, img2: &GrayImage) -> GrayImage {
+fn brightest<T: GenericImage>(img1: T, img2: T) -> GrayImage {
     GrayImage::from_fn(img1.width(), img1.height(), |x, y| {
         let p1 = img1.get_pixel(x, y);
         let p2 = img2.get_pixel(x, y);
-        if p1.0[0] < p2.0[0] {
-            *p2
+        if p1.to_luma()[0] < p2.to_luma()[0] {
+            // experimenting first with constant values before figuring out correct types
+            Luma([0u8]) //p2.to_luma()
         } else {
-            *p1
+            Luma([255u8]) //p1.to_luma()
         }
     })
 }
@@ -34,11 +35,13 @@ fn main() {
             let offset = 2u32.pow(round);
             let win_width = img.width() - offset;
             let win_height = img.height() - offset;
-            let orig = img.view(0, 0, win_width, win_height).to_image();
-            let left = img.view(offset, 0, win_width, win_height).to_image();
-            let up = img.view(0, offset, win_width, win_height).to_image();
-            let diag = img.view(offset, offset, win_width, win_height).to_image();
-            img = brightest(&brightest(&orig, &left), &brightest(&up, &diag));
+            let orig = img.view(0, 0, win_width, win_height);
+            let left = img.view(offset, 0, win_width, win_height);
+            let up = img.view(0, offset, win_width, win_height);
+            let diag = img.view(offset, offset, win_width, win_height);
+            let top_pixels = brightest(orig, left);
+            let bottom_pixels = brightest(up, diag);
+            img = brightest(top_pixels, bottom_pixels);
         }
         img.save(cli::get_out_fname(&f)).unwrap();
     }
